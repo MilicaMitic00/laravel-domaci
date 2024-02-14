87cf1c29d1365ce5931ea4c96c03585c401ad5b7
MilicaMitic00 <mitic.milica00@gmail.com>
2024-02-14 11:39:55 +0100

    Definisan je Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Agent;
+use \App\Models\Nekretnina;
+use \App\Models\Kandidat;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //brisanje svega iz tabela
+        User::truncate();
+        Agent::truncate();
+        Nekretnina::truncate();
+        Kandidat::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       User::factory(5)->create();
+       Agent::factory(10)->create();
+
+       $nekretnina1 = Nekretnina::create([
+           'naziv' => 'stan u Beogradu (Mirijevo)'
+       ]);
+
+       $nekretnina2 = Nekretnina::create([
+           'naziv' => 'kuća u Novom Sadu'
+       ]);
+
+       $nekretnina3 = Nekretnina::create([
+           'naziv' => 'apartman u Nišu'
+       ]);
+
+       $nekretnina4 = Nekretnina::create([
+           'naziv' => 'vikendica na Fruškoj Gori'
+       ]);
+
+       $nekretnina5 = Nekretnina::create([
+           'naziv' => 'poslovni prostor u Pirotu'
+       ]);
+
+       $kandidat1 = Kandidat::create([
+           'datumIVreme' => now(),
+           'user' => 1,
+           'nekretnina' => 1,
+           'agent'=>2,
+           'note' => 'Potencijalni kupac, ali želi dodatne informacije o poreklu vlasništva.'
+       ]);
+
+       $kandidat2 = Kandidat::create([
+           'datumIVreme' => now(),
+           'user' => 2,
+           'krivicnodelo' => 1,
+           'sudija'=>1,
+           'note' => 'Primetio je prisustvo vlage tokom obilaska, što će uticati na pregovore o ceni.'
+       ]);
     }
 }
